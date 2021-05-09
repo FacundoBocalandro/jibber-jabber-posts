118ea59f9ee361a4e9a30cfcb59890fac9dd42f8
Facundo Bocalandro <facundo@bocalandro.com.ar>
2021-05-09 17:32:33 -0300

    initial commit

---
+++ src/main/java/com/ingsis/jibberjabberposts/JibberJabberPostsApplication.java
@@ -0,0 +1,13 @@
+package com.ingsis.jibberjabberposts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibberJabberPostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibberJabberPostsApplication.class, args);
+	}
+
+}
